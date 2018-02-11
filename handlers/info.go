71fca539419b2734147fe86fd42d9e8cb46cbfa3
Tim Kellogg <tim.kellogg@gmail.com>
2018-02-11 09:55:25 -0800

    add explicit info response structure

--- handlers/info.go
+++ handlers/info.go
@@ -4,25 +4,29 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/timkellogg/five_three_one/services/exceptions"
+
 	"github.com/timkellogg/five_three_one/config"
 	"github.com/timkellogg/five_three_one/models"
-	"github.com/timkellogg/five_three_one/services/exceptions"
 )
+
+// InfoShowResponse - structure of InfoShow response
+type InfoShowResponse struct {
+	Version string
+}
 
 // InfoShow - prints out details about api
 func InfoShow(c *config.ApplicationContext, w http.ResponseWriter, r *http.Request) {
-	info := models.Info{
-		Major: models.Major,
-		Minor: models.Minor,
-		Patch: models.Patch,
+	responseStructure := InfoShowResponse{
+		Version: models.Major + "." + models.Minor + "." + models.Patch,
 	}
 
-	i, err := json.Marshal(info)
+	response, err := json.Marshal(responseStructure)
 	if err != nil {
 		handleError(err, exceptions.JSONParseError, w)
 		return
 	}
 
 	w.WriteHeader(http.StatusOK)
-	w.Write(i)
+	w.Write(response)
 }
--- handlers/info_test.go
+++ handlers/info_test.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"testing"
 )
@@ -11,7 +13,19 @@ func TestInfo(t *testing.T) {
 		t.Error(err)
 	}
 
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	var response InfoShowResponse
+	json.Unmarshal(body, &response)
+
 	if res.StatusCode != 200 {
 		t.Errorf("Info handler returned a non-200 status of %v", res.StatusCode)
+	}
+
+	if response.Version != "0.0.1" {
+		t.Errorf("Expected version to be 0.0.1 but was %s", response.Version)
 	}
 }
